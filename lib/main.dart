2f6da4b16303f842e84d4f1671113245723860cc
meganmaguire <meganmaguire000@gmail.com>
2019-12-05 17:42:11 -0300

    Agregados comentarios con la explicación de la tarea.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,16 @@ import 'MyApp.dart';
 
 int gridCount = 2;
 
+// La propuesta para este ejemplo de aplicación es que implementen alguna de las
+// pantallas que han quedado sin hacer. En la pestaña inferior de "TO-DO" se
+// tiene la lista de código a modificar para cada una de las pantallas. Elijan
+// alguna que sea de su interés y desarróllenla.
+
+// El proyecto está estructurado en clases por cada una de las pantallas posibles
+// sumado a una clase para el Widget que se diseñó para mostrar los artículos.
+
+// Ante cualquier duda no duden en escribirme.
+
 void main() => runApp(
 
 	// Esta es la aplicación principal.
